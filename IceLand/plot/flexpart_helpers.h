24e879a8daaf61bbbfdd6cd823c3e628a4904e42
Saji Hameed <saji.nh@gmail.com>
2011-08-17 21:28:08 +0900

    added plot

---
+++ IceLand/plot/flexpart_helpers.h
@@ -0,0 +1,12 @@
+#define NX 168
+#define NY 168
+#define NZ 18
+
+extern
+void skip_lines(FILE *fp, int nlines);
+extern
+void read_write(FILE *fp, FILE *out, int nlines);
+extern
+void skip_lines2(FILE *fp, int nlines);
+
+
